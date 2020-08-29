d7df4f584ba242ad9859452788bf2867a52381e6
Hayden <hayden@aitch.co>
2020-08-29 20:09:05 +1000

    added validation to res form

--- js/booking.js
+++ js/booking.js
@@ -1,3 +1,4 @@
+const form = document.querySelector('.reservations-form')
 const name = document.querySelector('#name')
 const email = document.querySelector('#email')
 const day = document.querySelector('#day')
@@ -11,6 +12,71 @@ const numOfPeople = document.querySelector('#num-of-people')
 const plural = document.querySelector('.numpicker-text')
 const minusButton = document.querySelector('#minus')
 const plusButton = document.querySelector('#plus')
+
+function checkPlural() {
+    if (numOfPeople.textContent === '1') {
+        plural.textContent = 'person'
+    } else {
+        plural.textContent = 'people'
+    }
+}
+
+function showError(input, message) {
+    const formControl = input.parentElement
+    formControl.classList.add('error')
+    const small = formControl.querySelector('small')
+    small.innerText = message
+}
+
+function showSuccess(input) {
+    const formControl = input.parentElement
+    formControl.classList.remove('error')
+    formControl.classList.add('success')
+}
+
+function checkEmail(input) {
+    const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (reg.test(input.value.trim())) {
+        showSuccess(input)
+    } else {
+        showError(input, 'Email is not valid')
+    }
+}
+
+function checkRequired(inputArray) {
+    inputArray.forEach( input => {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`)
+        } else {
+            showSuccess(input)
+        }
+    })
+}
+
+function checkCharLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    } else {
+        showSuccess(input)
+    }
+}
+
+function checkNumLength(input, min, max) {
+    if (input.value < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min}`)
+    } else if (input.value > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max}`)
+    } else {
+        showSuccess(input)
+    }
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
 
 minusButton.addEventListener('click', event => {
     event.preventDefault()
@@ -28,12 +94,15 @@ plusButton.addEventListener('click', event => {
     }
 })
 
-function checkPlural() {
-    if (numOfPeople.textContent === '1') {
-        plural.textContent = 'person'
-    } else {
-        plural.textContent = 'people'
-    }
-}
+form.addEventListener('submit', event => {
+    event.preventDefault()
 
-
+    checkRequired([name, email, day, month, year, minute, hour])
+    checkCharLength(name, 3, 25)
+    checkEmail(email)
+    checkNumLength(day, 1, 31)
+    checkNumLength(month, 1, 12)
+    checkNumLength(year, 2020, 2025)
+    checkNumLength(hour, 1, 12)
+    checkNumLength(minute, 00, 59)
+})
